0ee158e75fd278fedeadcf00de27580e468d5023
Gerardo Fernandez <gerardojose13@gmail.com>
2017-11-07 11:36:10 -0800

    pushes game draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,5 @@
 // ********** GAME ***************
-	// 1.- a pool of charachters not including letters or simbols
-	// 2.- var that will store user's input
-	// 3.- var that will generate a random computer letter
 	// 4.- if statements that will check if the computer character's was guessed or missed.
-	// 5.- vars that store wins, losses, guessed left, and guesses so far
 
 var letterPool = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -12,6 +8,38 @@ var losses = 0;
 var guessLeft = 12;
 var lettersGuessed = "";
 
+document.onkeyup = function(){
+	var computerGuess = letterPool[Math.floor(Math.random() * letterPool.length)];
 
+	var userInput = String.fromCharCode(event.keyCode).toLowerCase();
+
+	if (userInput){
+		guessLeft --;
+		lettersGuessed += userInput;
+		if (computerGuess === userInput){
+			wins ++;
+		}
+		if (computerGuess !== userInput){
+			losses++;
+		}
+	}
+
+	gameReset();
+
+	var html = "<p>Guess what letter I'm thinking of</p>" + "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>" + "<p>Guesses left: " + guessLeft + "</p>" + "<p>Your Guesses so far: " + lettersGuessed + "</p>";
+
+	document.querySelector("#dashboard").innerHTML = html;
+
+};
+
+
+function gameReset(){
+	if (guessLeft === 0){
+		wins = 0;
+		losses = 0;
+		guessLeft = 12;
+		lettersGuessed = "";
+	}
+}
 
 
