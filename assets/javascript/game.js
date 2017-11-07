f0669471f08640ef2ecd5bfb26acfc40a1f38f54
Gerardo Fernandez <gerardojose13@gmail.com>
2017-11-07 11:05:58 -0800

    pushes progress

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// ********** GAME ***************
+	// 1.- a pool of charachters not including letters or simbols
+	// 2.- var that will store user's input
+	// 3.- var that will generate a random computer letter
+	// 4.- if statements that will check if the computer character's was guessed or missed.
+	// 5.- vars that store wins, losses, guessed left, and guesses so far
+
+var letterPool = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var losses = 0;
+var guessLeft = 12;
+var lettersGuessed = "";
+
+
+
+
